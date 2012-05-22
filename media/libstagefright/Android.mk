7a0f596c9b12e2f5fa599794336550916055d5f1
Christian Troy <justcavalli@gmail.com>
2012-05-22 07:00:12 +0200

    added: stagefright acceleration

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -29,6 +29,7 @@ ifeq ($(BOARD_CAMERA_USE_MM_HEAP),true)
 endif
 endif
 include frameworks/base/media/libstagefright/codecs/common/Config.mk
+include external/cedarx/Config.mk
 
 LOCAL_SRC_FILES:=                         \
         ACodec.cpp                        \
@@ -108,7 +109,7 @@ LOCAL_C_INCLUDES+= \
         $(TOP)/frameworks/base/include/media/stagefright/openmax \
         $(TOP)/external/flac/include \
         $(TOP)/external/tremolo \
-        $(TOP)/external/openssl/include
+        $(TOP)/external/openssl/include \
 
 LOCAL_SHARED_LIBRARIES += \
         libbinder         \
@@ -136,7 +137,6 @@ LOCAL_STATIC_LIBRARIES := \
         libstagefright_timedtext \
         libvpx \
         libstagefright_mpeg2ts \
-        libstagefright_httplive \
         libstagefright_id3 \
         libFLAC \
 
@@ -152,6 +152,13 @@ endif
 
 ifeq ($(BOARD_USES_PROPRIETARY_OMX),SAMSUNG)
 LOCAL_CFLAGS     += -DSAMSUNG_OMX
+endif
+
+ifeq ($(CEDARX_DEBUG_FRAMEWORK),Y)
+LOCAL_STATIC_LIBRARIES += libstagefright_httplive_opt
+else
+LOCAL_LDFLAGS += \
+	$(CEDARX_TOP)/../CedarAndroidLib/LIB_ICS_$(CEDARX_CHIP_VERSION)/libstagefright_httplive_opt.a
 endif
 
 ################################################################################
